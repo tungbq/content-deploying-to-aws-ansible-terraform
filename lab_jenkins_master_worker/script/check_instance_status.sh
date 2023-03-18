5f3a222f060fbec6c656f2ed7d091fa9387fc0c1
Tung Bui <tung.bquang@gmail.com>
2023-03-18 15:25:23 +0700

    capture output even error happens

--- lab_jenkins_master_worker/script/check_instance_status.sh
+++ lab_jenkins_master_worker/script/check_instance_status.sh
@@ -2,8 +2,8 @@
 while true; do
   import_task_status_command="aws --profile $1 ec2 wait instance-status-ok --region $2 --instance-ids $3"
   echo "Running command: $import_task_status_command"
-  import_task_status=$($import_task_status_command)
-  echo "Import task [$import_task_id] status is [$import_task_status]."
+  import_task_status=$(${import_task_status_command} 2>&1)
+  echo "Status is [${import_task_status}]."
 
   if [[ "$import_task_status" == "" ]]; then
     echo "Completed, exiting..."
