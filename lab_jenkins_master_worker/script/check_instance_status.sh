91885e5b240230f5706597c918b299f808dc7994
Tung Bui <tung.bquang@gmail.com>
2023-03-18 15:28:03 +0700

    use retry instead of infinitive loop for checker

--- lab_jenkins_master_worker/script/check_instance_status.sh
+++ lab_jenkins_master_worker/script/check_instance_status.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-while true; do
+
+MAX_RETRIES=3
+RETRIES=0
+
+while [[ $RETRIES -lt $MAX_RETRIES ]]; do
   import_task_status_command="aws --profile $1 ec2 wait instance-status-ok --region $2 --instance-ids $3"
   echo "Running command: $import_task_status_command"
   import_task_status=$(${import_task_status_command} 2>&1)
@@ -9,6 +13,7 @@ while true; do
     echo "Completed, exiting..."
     break
   elif [[ "$import_task_status" == *"Max attempts exceeded"* ]]; then
+    RETRIES=$((RETRIES+1))
     echo "Time out, waiting 1 minute..."
     sleep 60
   else
